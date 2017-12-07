84369946ac1851587b675b4083dc65dac29d0d05
a1502549 <a1502549@LAB117M18.lab117.campus.ifsp>
2017-12-07 19:57:01 -0200

    att

--- proHouseDesktop/src/prohousedesktop/ProHouseDesktop.java
+++ proHouseDesktop/src/prohousedesktop/ProHouseDesktop.java
@@ -19,10 +19,14 @@ public class ProHouseDesktop {
      */
     public static void main(String[] args) {
         Equipamento E = new Equipamento();
-        ArrayList<Equipamento> lista = new ArrayList<Equipamento>();
-
-        System.out.println("Nome: " + E.getNome() + " IP: " + E.getIp());
-
+        ArrayList<Equipamento> lista = new ArrayList<>();
+        
+        E.setId(1);
+        E.setNome("CAFETEIRA");
+        E.setIp("10.10.117.117");
+        lista.add(E);
+        
+        lista.forEach(System.out::println);
     }
-
+    
 }
